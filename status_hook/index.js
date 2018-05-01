2c051d21cb0beff36c5ba3155f28d6532d734328
Brian Barnes <bgb@google.com>
2018-05-01 13:15:19 -0700

    removing hardcoded auth token

--- status_hook/index.js
+++ status_hook/index.js
@@ -1,50 +1,23 @@
-console.log("Starting cloud function");
 const octokit = require('@octokit/rest')();
-const Storage = require('@google-cloud/storage');
-const projectId = 'pso-data-ml';
-const gcs = new Storage({
-  projectId: projectId,
-});
-// TODO(bgb): Handle err.
-const file = gcs.bucket('ice-cream-sales-keys').file('github-token.txt.enc');
-file.download(function(err, contents) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const request = {
-    name: `projects/pso-data-ml/locations/global/keyRings/ice-cream-sales/cryptoKeys/github`,
-    resource: {
-      ciphertext: contents
-    }
-  };
-  // Decrypts the file using the specified crypto key
-  cloudkms.projects.locations.keyRings.cryptoKeys.decrypt(request, (err, response) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+const runtimeConfig = require('cloud-functions-runtime-config');
 
-    const result = response.data;
-    console.log(result.plaintext);
+runtimeConfig.getVariable('ice-cream-sales', 'github-token')
+  .then((github_token) => {
     // TODO(bgb): Should we authenticate on every request?
     octokit.authenticate({
       type: 'token',
-      token: result.plaintext
+      token: github_token
     });
+  })
+  .catch((err) => {
+    console.log(err);
+    return;
   });
-});
-
-console.log("Ending cloud function");
 
 module.exports.subscribe = (event, callback) => {
  const build = eventToBuild(event.data.data);
  console.log("Build results" + JSON.stringify(build));
 
-// Skip if the current status is not in the status list.
-// Add additional statues to list if you'd like:
-// QUEUED, WORKING, SUCCESS, FAILURE,
-// INTERNAL_ERROR, TIMEOUT, CANCELLED
   const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
   if (status.indexOf(build.status) === -1) {
     return callback();
